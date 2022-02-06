079c5599aa4248fe9246887af7cf256304e31fc8
Sean Park <chanseanpark@gmail.com>
2022-02-05 23:55:42 -0800

    working commands TODO: signals, background, and expansion

--- main.c
+++ main.c
@@ -10,224 +10,195 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-#define MAXLINE 2048  // Max number of characters in command
-#define MAXARGS 512  // Max number of arguments
-#define MAXPROCS 200  // Max number of processes
+# define MAXLINE 2048  // Limit of characters in user input
+# define MAXARGS 512  // Limit of args in user input
+# define MAXJOBS 200  // Limit of jobs in processes list
 
 // Global variables
-int foreground_mode = 0;  // 1 to set to foreground only mode
+int processes[MAXJOBS];
+int exit_status;
+int childStatus;
 
-struct command
-{
-    char *cmd;  // main command
-    char *argv[MAXARGS];  // arguments
-    int argc;  // number of arguments
-    char *input;  // Input file
-    char *output; // Output file
-    bool background;  // Boolean to indicate background execution
-};
+// Struct for processes
+typedef struct process {
+    pid_t pid;
+    char *cmd;
+    int status;
+    bool stopped;
+} process;
+
+// Struct for commands
+typedef struct command {
+    char *args[MAXARGS];
+    int argc;
+    char *infile;
+    char *outfile;
+    bool background;
+} command;
 
 /*
-*  Prompts the user for input and parses the command to store data in a struct
+*  Return user input string
 */
-void getInput(struct command *user_cmd)
-{
-    char input[MAXLINE];  // String array to hold user input
-    user_cmd->background = false;  // Initialize background to false
-    user_cmd->argc = 0;  // Initialize argc to 0
-
-    // Iterators for $$ variable expansion
-    int i;
-
-
+char *read_cmd(char *input) {
     // Prompt user for input
     printf(": ");
     fflush(stdout);
+
+    // Read user input
     fgets(input, MAXLINE, stdin);
 
-    // Remove newline character
+    // Remove newline characters
     input[strcspn(input, "\n")] = 0;
 
-    // If user enters nothing, update struct with empty string
-    if (strlen(input) == 0)
-    {
-        user_cmd->cmd = "";
-        return;
-    }
-
-    // If user enters a comment, update struct command with '#'
-    if (input[0] == '#')
-    {
-        user_cmd->cmd = "";
-        return;
-    }
-
-    // Parse input into struct
-    char *token = strtok(input, " ");
-    user_cmd->cmd = calloc(strlen(token) + 1, sizeof(char));
-    strcpy(user_cmd->cmd, token);
-
-    token = strtok(NULL, " ");
-
-    // Iterate through tokens and store in struct
-    while(token != NULL){
-        if (strcmp(token, "&") == 0 && input[strlen(input) - 1] != '&')  // Check for background execution and if its the last token
-        {
-            user_cmd->background = true;
-            token = strtok(NULL, " ");
-        } else if (strcmp(token, "<") == 0)  // Check for input redirection
-        {
-            token = strtok(NULL, " ");
-            user_cmd->input = calloc(strlen(token) + 1, sizeof(char));
-            strcpy(user_cmd->input, token);
-        } else if (strcmp(token, ">") == 0)  // Check for output redirection
-        {
-            token = strtok(NULL, " ");
-            user_cmd->output = calloc(strlen(token) + 1, sizeof(char));
-            strcpy(user_cmd->output, token);
-        } else {
-            // Replace every instance of $$ in the token with the pid of the process
-            for (i = 0; i < strlen(token); i++)
-            {
-                char *temp = strstr(token, "$$");
-
-                // If substring is found, replace token with string before substring concatenated with pid
-                if (temp)
-                {
-                    char pid[10];
-                    sprintf(pid, "%d", getpid());
-                    char *temp2 = calloc(strlen(token) + strlen(pid) + 1, sizeof(char));
-                    strncpy(temp2, token, temp - token);
-                    strcat(temp2, pid);
-                    strcat(temp2, temp + 2);
-                    token = temp2;
-                    free(temp2);
-                }
-            }
-            // Add token to args
-            user_cmd->argv[user_cmd->argc] = token;
-            user_cmd->argc++;
-        }
-        token = strtok(NULL, " ");
-        user_cmd->argv[user_cmd->argc] = NULL;  // Indicate end of args
-        return;
-    }
+    return input;
 }
 
 /*
-*  Executes the command stored in the struct other than built-in commands of exit, cd, and status by forking a child process
+*  Parse the input line into a command struct
 */
-void executeOtherCommand(struct command *user_cmd)
-{
-    int spawn_pid;
-    int status;
-    int fd_in;
-    int fd_out;
+command *parse_cmd(char *line) {
+    command *cmd = malloc(sizeof(command));
 
-    // Fork a child process
-    spawn_pid = fork();
+    char *token;
+    int i = 0;
 
-    switch(spawn_pid)
-    {
-        case -1:
-            perror("fork() failed!\n");
-            exit(1);
-            break;
-        case 0:
-            // If input redirection is specified, open the file and redirect input
-            if (strlen(user_cmd->input) > 0)
-            {
-                fd_in = open(user_cmd->input, O_RDONLY);
-                if (fd_in == -1)
-                {
+    // Set default values
+    cmd->infile = NULL;
+    cmd->outfile = NULL;
+    cmd->background = false;
+
+    // Split the input line into tokens
+    token = strtok(line, " ");
+    while (token != NULL) {
+        // Check for background, if found and it is the last token, set background to true
+        if (strcmp(token, "&") == 0 && i == strlen(line) - 1) {
+            cmd->background = true;
+            token = strtok(NULL, " ");
+        } else if (strcmp(token, ">") == 0) {
+            // Set the output file
+            cmd->outfile = strtok(NULL, " ");
+            token = strtok(NULL, " ");
+        } else if (strcmp(token, "<") == 0) {
+            // Set the input file
+            cmd->infile = strtok(NULL, " ");
+            token = strtok(NULL, " ");
+        } else {
+            // Add the token to the args array
+            cmd->args[i] = token;
+            i++;
+        }
+        token = strtok(NULL, " ");
+    }
+    cmd->argc = i;
+    return cmd;
+}
+
+/*
+*  Execute the command arguments
+*/
+void execute_cmd(command *cmd) {
+    int fd[2];  // 0 is read, 1 is write
+    pid_t spawnPid;
+
+    // Check if the command is a built-in command
+    if (strcmp(cmd->args[0], "exit") == 0) {
+        exit(0);
+    } else if (strcmp(cmd->args[0], "cd") == 0) {
+        // Check if the user is changing to the home directory
+        if (cmd->argc == 1) {
+            chdir(getenv("HOME"));
+        } else {
+            // Change to the directory specified
+            chdir(cmd->args[1]);
+        }
+    } else if (strcmp(cmd->args[0], "status") ==0) {
+        // Print the exit status or terminating signal of the last foreground process
+        if (WIFEXITED(childStatus)) {
+            printf("exit value %d\n", WEXITSTATUS(childStatus));
+        } else if (WIFSIGNALED(childStatus)) {
+            printf("terminated by signal %d\n", WTERMSIG(childStatus));
+        }
+    } else {
+        // Fork a child process
+        if ((spawnPid = fork()) == 0) {
+            // Check if the command has an input file
+            if (cmd->infile != NULL) {
+                // Open the file for reading
+                if ((fd[0] = open(cmd->infile, O_RDONLY)) == -1) {
                     perror("open");
                     exit(1);
                 }
-                dup2(fd_in, 0);
-                close(fd_in);
+                // Close stdin
+                close(0);
+                // Connect stdin to the input file
+                dup2(fd[0], 0);
+                // Close the input file
+                close(fd[0]);
             }
-
-            // If output redirection is specified, open the file and redirect output
-            if (strlen(user_cmd->output) > 0)
-            {
-                fd_out = open(user_cmd->output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                if (fd_out == -1)
-                {
+            // Check if the command has an output file
+            if (cmd->outfile != NULL) {
+                // Open the file for writing
+                if ((fd[1] = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1) {
                     perror("open");
                     exit(1);
                 }
-                dup2(fd_out, 1);
-                close(fd_out);
+                // Close stdout
+                close(1);
+                // Connect stdout to the output file
+                dup2(fd[1], 1);
+                // Close the output file
+                close(fd[1]);
             }
-
             // Execute the command
-            execvp(user_cmd->cmd, user_cmd->argv);
+            execvp(cmd->args[0], cmd->args);
+            // If the command is not found, print the error
             perror("execvp");
             exit(1);
-            break;
-        default:
-            // If background execution is specified, wait for the child process to finish
-            if (user_cmd->background)
-            {
-                waitpid(spawn_pid, &status, WNOHANG);
-                printf("Background pid is %d\n", spawn_pid);
-                fflush(stdout);
+        } else if (spawnPid < 0) {
+            perror("fork");
+            exit(1);
+        } else {
+            // Check if the command is a background process
+            if (cmd->background) {
+                // Add the child process to the processes list
+                processes[childStatus] = spawnPid;
             } else {
-                waitpid(spawn_pid, &status, 0);
+                // Wait for the child process to finish
+                waitpid(spawnPid, &childStatus, 0);
             }
+        }
     }
 }
 
 int main(void)
-{   
-    struct command user_cmd;  // Struct to hold user input
-    char cwd[MAXLINE];  // String array to hold current working directory
-
-    // Program name to indicate start of smallsh
+{
     printf("$ smallsh\n");
     fflush(stdout);
 
-    // Loop until user enters exit
-    while (true)
-    {
-        // Get user input
-        getInput(&user_cmd);
+    // Loop for user input
+    while (true) {
+        // Initialize string for user input
+        char *input = malloc(sizeof(char) * MAXLINE);
 
-        // If user_cmd is a comment or empty, continue to next iteration
-        if (strlen(user_cmd.cmd) == 0 || user_cmd.cmd == NULL)
-        {
+        // Initialize the command struct
+        command *cmd = malloc(sizeof(command));
+
+        // Read user input
+        read_cmd(input);
+
+        // If user input is empty or a comment, continue
+        if (input[0] == '\0' || input[0] == '#') {
             continue;
         }
 
-        // Check for built-in commands exit, cd, and status and always run in the foreground
-        // otherwise invoke executeOtherCommand to execute other comands by forking a child and using exec() family functions
-        if (strcmp(user_cmd.cmd, "exit") == 0)  // If user enters exit, exit
-        {
-            exit(0);
-        } else if (strcmp(user_cmd.cmd, "cd") == 0)  // If user enters cd, change directory
-        {
-           // If command is cd and there are no arguments, change to home directory
-              if (user_cmd.argc == 0)
-              {
-                chdir(getenv("HOME"));
-                // Print new working directory
-                getcwd(cwd, sizeof(cwd));
-                printf("%s\n", cwd);
-                fflush(stdout);
+        // Parse the input line into a command struct
+        cmd = parse_cmd(input);
 
-              } else {
-                // If command is cd and there are arguments, change to the specified directory
-                chdir(user_cmd.argv[0]);
-                getcwd(cwd, sizeof(cwd));
-                printf("%s\n", cwd);
-                fflush(stdout);
-                
-              }
-        } else  // Execute other commands
-        {
-            executeOtherCommand(&user_cmd);
-        }
+        // Execute user input
+        execute_cmd(cmd);
+
+        free(cmd);
+        free(input);
     }
     return 0;
 }
-
